f164e0a21a37a58e852d5dcd47efaa8e2dbdcde5
Balan Catalin <38920688+balan-catalin@users.noreply.github.com>
2020-12-01 21:04:23 +0200

    Add project files.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base((DbContextOptions) options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
